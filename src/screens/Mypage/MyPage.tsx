b181b9e3f5c68ebe2fb49e31dab4d16bb6e9745a
si-zero <snm306@naver.com>
2025-08-13 15:06:29 +0900

    feat: 마이 페이지 메뉴 영역 ( 최근 검색 기록 등 ) 생성

--- src/screens/Mypage/MyPage.tsx
+++ src/screens/Mypage/MyPage.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import MyPage_Menu from './MyPage_Menu';
 
 
 export default function MyPage() {
     return(
         <SafeAreaView className="flex-1 bg-white" edges={['top']}>
             <View>
-                <Text>Location Pagddde</Text>
+                <MyPage_Menu/>
             </View>
         </SafeAreaView>
             
