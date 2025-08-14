0664462a2e7c915107acc8e589f03c596aa2ca3d
si-zero <snm306@naver.com>
2025-08-14 16:43:38 +0900

    feat: 사용자 프로필 컴포넌트 구현

--- src/screens/Mypage/MyPage_Login.tsx
+++ src/screens/Mypage/MyPage_Login.tsx
@@ -9,7 +9,13 @@ export default function MyPage_Login() {
     <>
       <Header/>
       <View className="gap-3">
-        
+        <TouchableOpacity
+          onPress={() => console.log('Pressed')}
+          className="bg-white shadow-custom p-4 rounded-[15px] w-[342px] h-[137px]"
+        >
+          <Text className=" text-center text-black text-[17px] font-bold ">{user?.name}</Text>
+          <Text className=" text-center text-zinc-600 text-[10px] ">{user?.email}</Text>
+        </TouchableOpacity>
         <Text className="text-xl font-bold">{user?.name}님 환영합니다</Text>
         <TouchableOpacity className="bg-neutral-100 h-[44px] rounded-md items-center justify-center" onPress={logout}>
           <Text className="font-semibold">로그아웃</Text>
