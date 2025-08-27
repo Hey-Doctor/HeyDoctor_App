8c8f1318f973326fe4417d4fcdca9f11869b057f
si-zero <snm306@naver.com>
2025-08-27 11:34:36 +0900

    feat: 프로필 임시 이미지 추가

--- src/screens/Mypage/MyPage_Login.tsx
+++ src/screens/Mypage/MyPage_Login.tsx
@@ -1,27 +1,53 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useAuth } from '~/auth/AuthContext';
 import Header from './components/Header';
+import MyPage_Menu from './MyPage_Menu';
 
 export default function MyPage_Login() {
   const { user, logout } = useAuth();
   return (
     <>
+      <View className="flex-1 bg-white">
       <Header/>
-      <View className="gap-3">
+      <View className="flex-1 items-center justify-center gap-3">
         <TouchableOpacity
           onPress={() => console.log('Pressed')}
-          className="bg-white shadow-custom p-4 rounded-[15px] w-[342px] h-[137px]"
+          className="bg-white shadow-custom p-4 rounded-[15px] w-[345px] h-[137px] relative"
         >
-          <Text className=" text-center text-black text-[17px] font-bold ">{user?.name}</Text>
-          <Text className=" text-center text-zinc-600 text-[10px] ">{user?.email}</Text>
+          <Image
+            source={require('../../../assets/profile.jpeg')}
+            className="w-[125px] h-[125px] rounded-full absolute left-[8px] top-[6px]"
+          />
+          <Text className="text-black text-[17px] font-bold absolute left-[148px] top-[47px]">
+            {user?.name}
+          </Text>
+          <Text className="text-zinc-600 text-[10px] absolute left-[148px] top-[69px]">
+            {user?.email}
+          </Text>
+          <MaterialIcons name="chevron-right" className="absolute left-[315px] top-[58px]" size={20} />
         </TouchableOpacity>
-        <Text className="text-xl font-bold">{user?.name}님 환영합니다</Text>
-        <TouchableOpacity className="bg-neutral-100 h-[44px] rounded-md items-center justify-center" onPress={logout}>
-          <Text className="font-semibold">로그아웃</Text>
+
+
+        <View className="mt-6"> 
+          <MyPage_Menu/>
+        </View>
+
+        <Text className="font-extrabold mt-[36px]">ver 1.0.70</Text>
+
+        <TouchableOpacity
+          className="bg-component-color w-[100px] h-[28px] rounded-full flex-row items-center justify-center"
+          onPress={logout}
+        >
+          <Image
+            source={require('../../../assets/mascot_icon.png')}
+            className="w-[20px] h-[20px] mr-2"
+          />
+          <Text className="text-white font-semibold">로그아웃</Text>
         </TouchableOpacity>
-        
       </View>
+    </View>
     </>
   );
 }
--- src/screens/Mypage/components/Header.tsx
+++ src/screens/Mypage/components/Header.tsx
@@ -5,7 +5,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'; // 아이콘 - Mate
 
 export default function Header() {
   return (
-    <SafeAreaView className="flex-1 bg-white" edges={['top']}>
+    <SafeAreaView className="bg-white" edges={['top']}>
         <View className="px-5 py-4 border-b border-neutral-200 flex-row justify-between">
             <Text className="text-xl font-bold">MY</Text>
             <MaterialCommunityIcons name="cog" size={22} />
