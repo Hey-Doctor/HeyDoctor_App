46fed84e812df3b3255b6182140158f75a92dee6
Youwol <153346797+si-zero@users.noreply.github.com>
2025-09-08 15:42:23 +0900

    [refactor] 홈 페이지 코드 구조 변경

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,27 +1,14 @@
 import { Image, View } from 'react-native';
 import Home_Main from './Home_Main';
 import Home_Sub from './Home_Sub';
-
+import Home_Header from './Home_Header';
 import { NavButton } from '~/components/NavButton';
 
 export default function Home() {
   return (<>
   <View className='flex-1 flex-col justify-center gap-4 p-7'>
-    {/* 맨 위에 칸 */}
-    <View className='w-[351px] h-[75px] p-4 flex-row items-center justify-between bg-[#F1FAF1] rounded-2xl'>
-      <Image 
-        source={require('~/assets/screens/HomeAssets/heyDoctorSmallLOGO1.png')} />
 
-      <View className='flex-row gap-2 items-center'>
-        <View className='w-[40px] h-[40px] rounded-full bg-[#82CD7B] flex items-center justify-center'>
-          <Image 
-            source={require('~/assets/screens/HomeAssets/customer1.png')} 
-            className='w-[25px] h-[25px]' />
-        </View>
-        <Image 
-        source={require('~/assets/screens/HomeAssets/alarmButton1.png')} />
-      </View>
-    </View>
+      <Home_Header/>
 
       <Home_Main />
       <Home_Sub />
