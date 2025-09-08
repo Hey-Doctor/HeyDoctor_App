8150eebc9344eda20ba6202e82a27851056a8d7e
si-zero <snm306@naver.com>
2025-09-08 14:23:50 +0900

    feat: IOS / Android 기기 별 그림자 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
       boxShadow: {
         custom: '0px 4px 4px rgba(0, 0, 0, 0.25)',
       },
+      elevation: {
+        // Android 전용 그림자
+        custom: 4,
+      },
     },
   },
   presets: [require('nativewind/preset')], // 반드시 포함해야 NativeWind 작동
